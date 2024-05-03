b16f938d91ea5a2c84a360a8242e36896e91597b
kimminseop99 <minseopkim99@gmail.com>
2024-05-03 15:45:30 +0900

    Answer Entity

---
+++ src/main/java/com/sbs/sbb/Answer.java
@@ -0,0 +1,25 @@
+package com.sbs.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity // answer table
+public class Answer {
+    @Id // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT") // TEXT
+    private String content;
+
+    private LocalDateTime createDate;
+
+    // private Integer questionId
+    @ManyToOne // table의 관계에서 반드시 작성해주어야함
+    private Question question;
+}
