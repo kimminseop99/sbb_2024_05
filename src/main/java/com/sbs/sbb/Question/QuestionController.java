cb45ca3194415c9ff15fa2f20792b7b1e058b0aa
kimminseop99 <minseopkim99@gmail.com>
2024-05-08 15:16:00 +0900

    질문 ã리스트, Model을 이용해 View로 데이터 전달

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -1,14 +1,24 @@
 package com.sbs.sbb.Question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
 
-    @GetMapping("/question/list")
+    private final QuestionRepository questionRepository;
 
-    public String list() {
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+
         return "question_list";
     }
 }
