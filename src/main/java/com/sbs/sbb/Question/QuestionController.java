1d0d7099a7874a44e47c0e4a5f002ac57e29489b
kimminseop99 <minseopkim99@gmail.com>
2024-05-08 17:49:18 +0900

    답변 등록 폼 생성

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -3,9 +3,7 @@ package com.sbs.sbb.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +32,12 @@ public class QuestionController {
 
 
         return "question_detail";
+    }
+
+
+    @PostMapping("/createTemp")
+    @ResponseBody
+    public String temp(){
+        return "전송완료";
     }
 }
