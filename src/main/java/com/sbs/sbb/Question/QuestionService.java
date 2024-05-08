ac0b6a778576ae596387f80fe77f918bc21657f1
kimminseop99 <minseopkim99@gmail.com>
2024-05-08 16:14:45 +0900

    QuestionService 추가

---
+++ src/main/java/com/sbs/sbb/Question/QuestionService.java
@@ -0,0 +1,16 @@
+package com.sbs.sbb.Question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> findAll() {
+        return questionRepository.findAll();
+    }
+}
