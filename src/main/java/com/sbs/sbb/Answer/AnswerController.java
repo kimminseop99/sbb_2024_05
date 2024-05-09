37fee8d782ddafe36fd4732ccbede0b8181a21a7
kimminseop99 <minseopkim99@gmail.com>
2024-05-09 14:29:22 +0900

    답변 저장 기능 추가

--- src/main/java/com/sbs/sbb/Answer/AnswerController.java
+++ src/main/java/com/sbs/sbb/Answer/AnswerController.java
@@ -1,5 +1,6 @@
 package com.sbs.sbb.Answer;
 
+import ch.qos.logback.core.joran.util.beans.BeanDescriptionFactory;
 import com.sbs.sbb.Question.Question;
 import com.sbs.sbb.Question.QuestionService;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +16,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class AnswerController {
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value = "content") String content){
         // 답변 부모 질문객체를 받아온다.
         Question q = this.questionService.getQuestion(id);
+
+        Answer answer = this.answerService.create(q, content);
 
         return String.format("redirect:/question/detail/%s", id);
     }
--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -1,19 +1,13 @@
 package com.sbs.sbb;
 
-import com.sbs.sbb.Answer.Answer;
 import com.sbs.sbb.Answer.AnswerRepository;
-import com.sbs.sbb.Question.Question;
 import com.sbs.sbb.Question.QuestionRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -27,7 +21,7 @@ class SbbApplicationTests {
 	@Transactional
 	@Test
 	void testJpa() {
-		/*Question q1 = new Question();
+		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
@@ -37,18 +31,21 @@ class SbbApplicationTests {
 		q2.setSubject("스프링부트 모델 질문입니다.");
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장*/
+		this.questionRepository.save(q2);  // 두번째 질문 저장
+
 		/*List<Question> all = this.questionRepository.findAll();
 		assertEquals(2, all.size());
 
 		Question q = all.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());*/
 
+
 		/*Optional<Question> oq = this.questionRepository.findById(1);
 		if(oq.isPresent()) {
 			Question q = oq.get();
 			assertEquals("sbb가 무엇인가요?", q.getSubject());
 		}*/
+
 
 		/*Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
 		assertEquals(1, q.getId());*/
@@ -85,13 +82,13 @@ class SbbApplicationTests {
 		Answer a = oa.get();
 		assertEquals(2, a.getQuestion().getId());*/
 
-		Optional<Question> oq = this.questionRepository.findById(2);
+		/*Optional<Question> oq = this.questionRepository.findById(2);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 
 		List<Answer> answerList = q.getAnswerList();
 
 		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());*/
 	}
 }
