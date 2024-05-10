b68de34b8975cd2048b2d7dcca1348b9a0b019a0
kimminseop99 <minseopkim99@gmail.com>
2024-05-10 17:46:03 +0900

    페이징 구현

--- src/main/java/com/sbs/sbb/Question/QuestionRepository.java
+++ src/main/java/com/sbs/sbb/Question/QuestionRepository.java
@@ -1,5 +1,7 @@
 package com.sbs.sbb.Question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
@@ -8,4 +10,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     Question findBySubjectAndContent(String subject, String content);
 
+    Page<Question> findAll(Pageable pageable);
 }
