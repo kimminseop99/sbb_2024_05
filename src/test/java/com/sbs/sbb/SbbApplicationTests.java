f638aa124c101092a0e5e6ae31235fcf75f5711f
kimminseop99 <minseopkim99@gmail.com>
2024-05-16 14:24:45 +0900

    ívoters entity추가

--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -67,6 +67,22 @@ class SbbApplicationTests {
 
 		// 답변 1개 생성
 		Answer a1 = answerService.create(q2, "네 자동으로 생성됩니다.", user2);
+
+		// 1번 질문에 2명의 회원이 추천을 한다.
+		// user1 (이)가 q1 (을)를 추천했다.
+		q1.addVoter(user1);
+		q1.addVoter(user2);
+		questionRepository.save(q1);
+
+		// 2번 질문에 2명의 회원이 추천을 한다.
+		q2.addVoter(user1);
+		q2.addVoter(user2);
+		questionRepository.save(q2);
+
+		// 1번 답변에 2명의 회원이 추천을 한다.
+		a1.addVoter(user1);
+		a1.addVoter(user2);
+		answerRepository.save(a1);
 	}
 
 	@Test
